ab263947fb27296d0ab3ec88237232bcdff6efa1
Matheus Pereira <matheus.p.soares242@gmail.com>
2025-04-08 16:03:27 -0300

    Documentação

--- docs/database/README.md
+++ docs/database/README.md
@@ -1,4 +1,4 @@
 ## 🛠 Como Rodar o Projeto
 
 1. Crie um banco de dados MySQL chamado `salao_leila`
-2. Importe o arquivo `database.sql`
+2. Importe o arquivo `new_database.sql`
--- docs/database/new_database.sql
+++ docs/database/new_database.sql
@@ -46,16 +46,22 @@ CREATE TABLE IF NOT EXISTS `agendamentos` (
   PRIMARY KEY (`id`),
   KEY `cliente_id` (`cliente_id`),
   CONSTRAINT `agendamentos_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Copiando dados para a tabela salao_leila.agendamentos: ~6 rows (aproximadamente)
+-- Copiando dados para a tabela salao_leila.agendamentos: ~12 rows (aproximadamente)
 INSERT INTO `agendamentos` (`id`, `cliente_id`, `data_agendamento`, `STATUS`, `data_solicitacao`) VALUES
 	(31, 4, '2025-03-19 00:00:00', 'Pendente', '2025-03-16 14:16:04'),
 	(35, 3, '2025-03-25 00:00:00', 'Pendente', '2025-03-16 19:02:09'),
-	(36, 3, '2025-03-17 00:00:00', 'Pendente', '2025-03-16 19:12:31'),
 	(37, 2, '2025-03-19 00:00:00', 'Pendente', '2025-03-17 16:16:11'),
 	(41, 6, '2025-03-28 00:00:00', 'Pendente', '2025-03-17 17:45:48'),
-	(42, 6, '2025-04-02 00:00:00', 'Pendente', '2025-03-17 17:46:27');
+	(42, 6, '2025-04-02 00:00:00', 'Pendente', '2025-03-17 17:46:27'),
+	(45, 7, '2025-04-04 00:00:00', 'Pendente', '2025-03-31 12:04:57'),
+	(47, 3, '2025-04-03 00:00:00', 'Pendente', '2025-03-31 12:39:02'),
+	(50, 8, '2025-04-03 00:00:00', 'Pendente', '2025-03-31 13:46:08'),
+	(51, 2, '2025-04-07 14:30:00', 'Realizado', '2025-04-07 13:31:26'),
+	(52, 6, '2025-04-08 14:16:00', 'Pendente', '2025-04-08 14:05:40'),
+	(55, 2, '2025-04-10 16:30:00', 'Realizado', '2025-04-08 17:20:01'),
+	(60, 3, '2025-04-08 15:00:00', 'Pendente', '2025-04-08 17:34:34');
 
 -- Copiando estrutura para tabela salao_leila.agendamento_servicos
 CREATE TABLE IF NOT EXISTS `agendamento_servicos` (
@@ -67,16 +73,17 @@ CREATE TABLE IF NOT EXISTS `agendamento_servicos` (
   KEY `servico_id` (`servico_id`),
   CONSTRAINT `agendamento_servicos_ibfk_1` FOREIGN KEY (`agendamento_id`) REFERENCES `agendamentos` (`id`) ON DELETE CASCADE,
   CONSTRAINT `agendamento_servicos_ibfk_2` FOREIGN KEY (`servico_id`) REFERENCES `servicos` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Copiando dados para a tabela salao_leila.agendamento_servicos: ~6 rows (aproximadamente)
+-- Copiando dados para a tabela salao_leila.agendamento_servicos: ~7 rows (aproximadamente)
 INSERT INTO `agendamento_servicos` (`id`, `agendamento_id`, `servico_id`) VALUES
-	(24, 31, 1),
-	(29, 35, 1),
-	(30, 36, 3),
 	(31, 37, 2),
-	(35, 41, 1),
-	(36, 42, 4);
+	(36, 42, 4),
+	(44, 50, 4),
+	(45, 51, 4),
+	(46, 52, 3),
+	(49, 55, 2),
+	(54, 60, 2);
 
 -- Copiando estrutura para tabela salao_leila.clientes
 CREATE TABLE IF NOT EXISTS `clientes` (
@@ -88,30 +95,33 @@ CREATE TABLE IF NOT EXISTS `clientes` (
   `data_cadastro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Copiando dados para a tabela salao_leila.clientes: ~4 rows (aproximadamente)
+-- Copiando dados para a tabela salao_leila.clientes: ~6 rows (aproximadamente)
 INSERT INTO `clientes` (`id`, `nome`, `telefone`, `senha`, `email`, `data_cadastro`) VALUES
 	(2, 'João Oliveira', '987654321', '3d7fcc75ff6bfcbc40127078aa3760d5', 'joao@email.com', '2025-03-14 14:02:08'),
 	(3, 'Matheus', '123456789', 'e7d80ffeefa212b7c5c55700e4f7193e', 'matheus@email.com', '2025-03-14 17:59:03'),
 	(4, 'Lucas', '99999-9999', '25d55ad283aa400af464c76d713c07ad', 'lucas@email.com', '2025-03-16 14:44:11'),
-	(6, 'Matheus Pereira Soares', '14997906348', 'e7614d62a71b71cb13e8f45d0b7e5713', 'matheus.psoares4@gmail.com', '2025-03-17 17:44:34');
+	(6, 'Matheus Pereira Soares', '14997906348', 'e7614d62a71b71cb13e8f45d0b7e5713', 'matheus.psoares4@gmail.com', '2025-03-17 17:44:34'),
+	(7, 'Rogerio', '99999-9999', 'e10adc3949ba59abbe56e057f20f883e', 'rogerio@email.com', '2025-03-19 21:20:25'),
+	(8, 'Matheus', '999999999', 'e7d80ffeefa212b7c5c55700e4f7193e', 'matheusmpz@gmail.com', '2025-03-31 13:43:52');
 
 -- Copiando estrutura para tabela salao_leila.servicos
 CREATE TABLE IF NOT EXISTS `servicos` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `preco` decimal(10,2) NOT NULL,
-  `descricao` varchar(136) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descricao` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `imagem` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Copiando dados para a tabela salao_leila.servicos: ~4 rows (aproximadamente)
-INSERT INTO `servicos` (`id`, `nome`, `preco`, `descricao`) VALUES
-	(1, 'Corte de Cabelo Feminino', 65.00, 'Corte personalizado para cada tipo de cabelo, garantindo leveza, movimento e elegância.'),
-	(2, 'Manicure', 50.00, 'Unhas bem cuidadas com esmaltação perfeita e acabamento impecável para um toque de sofisticação.'),
-	(3, 'Corte de Cabelo Masculino', 35.00, 'Do clássico ao degradê, um corte impecável e alinhado às tendências para realçar seu estilo.'),
-	(4, 'Hidratação', 120.00, 'Nutrição profunda para cabelos sedosos, brilhantes e saudáveis, restaurando a hidratação essencial dos fios.\n\n');
+INSERT INTO `servicos` (`id`, `nome`, `preco`, `descricao`, `imagem`) VALUES
+	(2, 'Manicure', 50.00, 'Unhas bem cuidadas com esmaltação perfeita e acabamento impecável para um toque de sofisticação.', 'uploads/servicos/peter-kalonji-LH74lRYvBY4-unsplash.jpg'),
+	(3, 'Corte de Cabelo Masculino', 35.00, 'Do clássico ao degradê, um corte impecável e alinhado às tendências para realçar seu estilo.', 'uploads/servicos/sunny-ng-KVIlNRoGwxk-unsplash.jpg'),
+	(4, 'Hidratação', 120.00, 'Nutrição profunda para cabelos sedosos, brilhantes e saudáveis, restaurando a hidratação essencial dos fios.\r\n\r\n', 'uploads/servicos/peter-kalonji-LH74lRYvBY4-unsplash.jpg'),
+	(6, 'Lavagem', 45.00, 'Lavagem ideal para seu cabelo!', 'uploads/servicos/sunny-ng-KVIlNRoGwxk-unsplash.jpg');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
